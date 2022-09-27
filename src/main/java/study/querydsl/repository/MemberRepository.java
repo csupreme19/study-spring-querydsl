0797730c3782a5dd2a935efe28c05377fa75900f
csupreme19 <csupreme19@gmail.com>
2022-09-28 01:52:23 +0900

    QuerydslPredicateExecutor 사용해보기

--- src/main/java/study/querydsl/repository/MemberRepository.java
+++ src/main/java/study/querydsl/repository/MemberRepository.java
@@ -1,11 +1,12 @@
 package study.querydsl.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import study.querydsl.entity.Member;
 
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberCustomRepository {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberCustomRepository, QuerydslPredicateExecutor<Member> {
 
     // Spring Data JPA가 JPQL 자동 생성
     // select m from Member m where m.username = :username
